c12c438101afd615e0a698facbf1b0ac07a46d4a
Laura Galvis <laura66gb@gmail.com>
2024-10-26 15:22:55 -0500

    Integración backend y frontend Tratamiento

--- client/src/components/Tratamiento/index.tsx
+++ client/src/components/Tratamiento/index.tsx
@@ -1,10 +1,49 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./tratamiento.css"; //Estilos
 import { Link } from "react-router-dom"; //Rutas
 import { GoHome, GoSearch, GoGear } from "react-icons/go";
 import { PiPawPrint } from "react-icons/pi";
 
 const Tratamiento: React.FC = () => {
+  const [mascota, setMascota] = useState("");
+  const [nombre, setNombre] = useState("");
+  const [fecha, setFecha] = useState("");
+  const [precio, setPrecio] = useState("");
+  const [detalles, setDetalles] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDetalles(e.target.value);
+  };
+
+  const handleSubmit = async () => {
+    const data = { mascota, nombre, fecha, precio, detalles };
+
+    try {
+      const response = await fetch("http://localhost:3000/treatment/addtreat", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      const result = await response.json();
+      if (response.ok) {
+        alert(result.message);
+        // Limpiar el formulario si el tratamiento fue agregado exitosamente
+        setMascota("");
+        setNombre("");
+        setFecha("");
+        setPrecio("");
+        setDetalles("");
+      } else {
+        alert("Error: " + result.message);
+      }
+    } catch (error) {
+      console.error("Error al agregar el tratamiento:", error);
+    }
+  };
+
   return (
     <div className="treat-box">
       <div className="headerp">
@@ -25,65 +64,77 @@ const Tratamiento: React.FC = () => {
         </div>
       </div>
       <div className="wrappert">
-        <div className="addtreat">
-          <form className="formt">
+        <form
+          className="formt"
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+        >
+          <div className="addtreat">
             <h1>Tratamiento</h1>
             <div className="input-boxt">
               <label className="titulos">Mascota:</label>
               <input
-                type="search"
-                id="mascotareg"
-                name="mascotareg"
-                placeholder="Buscar registro de mascota"
+                type="text"
+                value={mascota}
+                onChange={(e) => setMascota(e.target.value)}
+                required
+                placeholder="Ingresar nombre de mascota registrada"
               />
             </div>
             <div className="input-boxt">
               <label className="titulos">Tratamiento:</label>
-
               <input
                 type="text"
-                id="treat"
-                name="treat"
-                placeholder="Ingresar tratamiento"
+                value={nombre}
+                onChange={(e) => setNombre(e.target.value)}
+                required
+                placeholder="Ingresar nombre del tratamiento"
               />
             </div>
             <div className="input-boxt">
               <label className="titulos">Fecha:</label>
-
               <input
                 type="date"
-                id="date"
-                name="date"
+                value={fecha}
+                onChange={(e) => setFecha(e.target.value)}
+                required
                 placeholder="Ingresar fecha"
               />
             </div>
-          </form>
-        </div>
-        <div className="addtreat">
-          <form className="formt">
-            <div className="input-boxt">
-              <label className="titulos">Código o costo:</label>
+          </div>
 
+          <div className="addtreat">
+            <div className="input-boxt">
+              <label className="titulos">Precio:</label>
               <input
                 type="number"
-                id="cod"
-                name="cod"
-                placeholder="Ingresar codigo o costo"
+                value={precio}
+                onChange={(e) => setPrecio(e.target.value)}
+                required
+                placeholder="Ingresar Precio"
               />
             </div>
             <div className="input-boxt">
-              <label className="titulos">Detalles</label>
+              <label className="titulos">Detalles:</label>
+              <textarea
+                id="detalles"
+                value={detalles}
+                onChange={handleChange}
+                placeholder="Escribe aquí los síntomas"
+              />
             </div>
             <div className="botont">
-              <button className="uno" type="button">
+              <button className="uno" type="submit">
                 Continuar
               </button>
               <button className="dos" type="reset">
                 Cancelar
               </button>
             </div>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
     </div>
   );
